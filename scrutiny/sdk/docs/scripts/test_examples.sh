765a1349d671c4b7048d4511312beadf6067c727
Pier-Yves Lessard <py.lessard@gmail.com>
2025-10-26 17:32:36 -0400

    Updated doc mypy config to match project

--- scrutiny/sdk/docs/scripts/test_examples.sh
+++ scrutiny/sdk/docs/scripts/test_examples.sh
@@ -11,6 +11,7 @@ set -x
 echo -e "\nTesting example code..."
 
 cd $tempdir
+MYPY_OPTIONS="--no-warn-redundant-casts --strict --cache-dir $tempdir"
 
 # HIL testing
 cd $EXAMPLES_ROOT/hil_testing
@@ -18,7 +19,7 @@ outfile="$tempdir/hil_testing.cpp"
 cat *.cpp > $outfile
 g++ -c "$outfile" -o "$tempdir/hil_testing.o"
 g++ -c "$outfile" -o "$tempdir/hil_testing.o" -DENABLE_HIL_TESTING 
-python3 -m mypy --cache-dir $tempdir hil_testing_1_powerup_check.py --strict
+python3 -m mypy hil_testing_1_powerup_check.py $MYPY_OPTIONS
 
 # EOL Config
 cd $EXAMPLES_ROOT/eol_config
@@ -26,8 +27,8 @@ outfile="$tempdir/eol_config.cpp"
 cat *.cpp > $outfile
 g++ -c "$outfile" -o $tempdir/eol_config.o
 g++ -c "$outfile" -o $tempdir/eol_config.o -DENABLE_EOL_CONFIGURATOR
-python3 -m mypy --cache-dir $tempdir eol_config_assembly_header.py --strict
-python3 -m mypy --cache-dir $tempdir eol_config_dump_eeprom.py --strict
+python3 -m mypy eol_config_assembly_header.py $MYPY_OPTIONS
+python3 -m mypy eol_config_dump_eeprom.py $MYPY_OPTIONS
 
 # Calibration
 cd $EXAMPLES_ROOT/calibration
@@ -35,16 +36,16 @@ outfile="$tempdir/calibration.cpp"
 cat *.cpp > $outfile
 g++ -c "$outfile" -o $tempdir/calibration.o
 g++ -c "$outfile" -o $tempdir/calibration.o -DENABLE_TUNNING
-python3 -m mypy --cache-dir $tempdir calibration_1_pi_graph.py --strict 
+python3 -m mypy calibration_1_pi_graph.py $MYPY_OPTIONS
 
 
 # Event looping
 cd $EXAMPLES_ROOT/event_looping
-python3 -m mypy --cache-dir $tempdir event_looping.py --strict 
+python3 -m mypy event_looping.py $MYPY_OPTIONS
 
 # SFD upload/download
 cd $EXAMPLES_ROOT/sfd_upload_download
-python3 -m mypy --cache-dir $tempdir download_sfd.py --strict 
-python3 -m mypy --cache-dir $tempdir upload_sfd.py --strict 
+python3 -m mypy download_sfd.py $MYPY_OPTIONS
+python3 -m mypy upload_sfd.py $MYPY_OPTIONS
 
 rm -rf $tempdir
