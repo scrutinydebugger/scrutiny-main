7ec06e248941f3dac9199556289fce9f512b9385
Pier-Yves Lessard <py.lessard@gmail.com>
2025-11-09 19:12:33 -0500

    Improve dark theme

--- test/gui/manual/manual_test_stylesheets.py
+++ test/gui/manual/manual_test_stylesheets.py
@@ -1,3 +1,11 @@
+#    manual_test_stylesheets.py
+#        A window to visualize different custom stylesheets features
+#
+#   - License : MIT - See LICENSE file.
+#   - Project :  Scrutiny Debugger (github.com/scrutinydebugger/scrutiny-main)
+#
+#   Copyright (c) 2025 Scrutiny Debugger
+
 if __name__ != '__main__':
     raise RuntimeError("This script is expected to run from the command line")
 
