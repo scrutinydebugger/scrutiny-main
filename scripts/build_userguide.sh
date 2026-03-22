82cd1c43234721f757b9243152647945242d417e
Pier-Yves Lessard <py.lessard@gmail.com>
2026-03-21 21:35:50 -0400

    fixed userguide script

--- scripts/build_userguide.sh
+++ scripts/build_userguide.sh
@@ -6,12 +6,12 @@ PROJECT_ROOT="$(get_project_root)"
 cd "${PROJECT_ROOT}"
 
 export PYTHONPATH="$PROJECT_ROOT:${PYTHONPATH:-}"
-info "Building $FILENAME.pdf"
+USER_GUIDE_PDF_DST=$(python -m scrutiny userguide location --nocheck)
+info "Building $(basename "$USER_GUIDE_PDF_DST").pdf"
 
 SPHINXOPTS=-W make -C docs/user_guide latexpdf
 
 OUTPUT_DIR="$PROJECT_ROOT/docs/user_guide/build/latex"
-USER_GUIDE_PDF_DST=$(python -m scrutiny userguide location --nocheck)
 USER_GUIDE_PDF_SRC="$OUTPUT_DIR/$(basename "$USER_GUIDE_PDF_DST")"
 assert_file "$USER_GUIDE_PDF_SRC"
 rm -f $USER_GUIDE_PDF_DST
