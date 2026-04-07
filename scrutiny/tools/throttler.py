1bff8c73ee9970e2be4876f9c31a462a41ffa3d3
Pier-Yves Lessard <py.lessard@gmail.com>
2026-04-06 20:28:04 -0400

    mypy

--- scrutiny/tools/throttler.py
+++ scrutiny/tools/throttler.py
@@ -57,7 +57,7 @@ class Throttler:
     consumed_since_last_estimation: int
     """Amount consumed since last estimation."""
 
-    def __init__(self, mean_rate, estimation_window: float = 0.1, slow_tau: float = 1.0, fast_tau=0.05) -> None:
+    def __init__(self, mean_rate: float, estimation_window: float = 0.1, slow_tau: float = 1.0, fast_tau: float = 0.05) -> None:
         self.enabled = False
         self.mean_rate = float(mean_rate)
         self.estimation_window_ns = int(round(estimation_window * 1e9))
