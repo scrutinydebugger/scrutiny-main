scrutinydebugger/scrutiny-main

commit 91c52b7ce555473fd457b0ae49f447208bac2242

Support variable rate watch (#319)
- Add ability to limit the polling rate of individual watchable in the server to reduce the bandwidth between the server and the device.
- Added facilities in the SDK to exploit this new feature

scrutiny/server/device/submodules/memory_reader.py
@@ -26,11 +26,13 @@ import scrutiny.server.protocol.commands as cmd
 import scrutiny.server.protocol.typing as protocol_typing
 from scrutiny.server.device.request_dispatcher import RequestDispatcher
 from scrutiny.server.datastore.datastore import Datastore
-from scrutiny.server.datastore.datastore_entry import DatastoreVariableEntry, DatastorePointedVariableEntry, DatastoreRPVEntry, DatastoreEntryInvalidReason
+from scrutiny.server.datastore.datastore_entry import (DatastoreVariableEntry, DatastorePointedVariableEntry,
+                                                       DatastoreRPVEntry, DatastoreEntryInvalidReason, DatastoreEntry)
 from scrutiny.core.memory_content import MemoryContent, Cluster
 from scrutiny.core.basic_types import MemoryRegion
 from scrutiny.tools.queue import ScrutinyQueue
 from scrutiny import tools
+from scrutiny.tools.throttler import Throttler
 
 from scrutiny.tools.typing import *
 
@@ -161,6 +163,7 @@ class MemoryReader(BaseDeviceHandlerSubmodule):
     """Reraise errors that we normally hide to catch them in unit testing"""
     read_type_changed: bool
     """A flag that indicates if the read type change in the last process iteration"""
+    entry_to_throttler_map: Dict[DatastoreEntry, Throttler]
 
     def __init__(self, protocol: Protocol, dispatcher: RequestDispatcher, datastore: Datastore, request_priority: int):
         self.logger = logging.getLogger(self.__class__.__name__)
@@ -170,11 +173,19 @@ class MemoryReader(BaseDeviceHandlerSubmodule):
         self.request_priority = request_priority
         self.datastore.add_watch_callback(self._the_watch_callback)
         self.datastore.add_unwatch_callback(self._the_unwatch_callback)
+        self.datastore.add_update_rate_change_callback(self._the_update_rate_changed_callback)
         self.active_raw_read_request = None
         self.raw_read_request_queue = ScrutinyQueue()
         self.unittest_crash_on_critical_error = False
+        self.entry_to_throttler_map = {}
 
         self.reset()
+
+    def get_throttler_rate(self, entry: DatastoreEntry) -> Optional[float]:
+        """Return the throttling rate of the given entry. Return ``None`` if not throttled (even if not watched)"""
+        if entry not in self.entry_to_throttler_map:
+            return None
+        return self.entry_to_throttler_map[entry].get_rate()
 
     def set_max_request_payload_size(self, max_size: int) -> None:
         """Set the maximum payload size that can be sent in a request. Depends on device internal buffer size"""
@@ -204,6 +215,20 @@ class MemoryReader(BaseDeviceHandlerSubmodule):
         self.raw_read_request_queue.put_nowait(request)
         return request
 
+    def _update_entry_update_rate(self, entry: DatastoreEntry, rate: Optional[float]) -> None:
+        if rate is None:
+            with tools.SuppressException(KeyError):
+                del self.entry_to_throttler_map[entry]
+        else:
+            if entry not in self.entry_to_throttler_map:
+                self.entry_to_throttler_map[entry] = Throttler(rate, estimation_window=0.01)
+                self.entry_to_throttler_map[entry].enable()
+            throttler = self.entry_to_throttler_map[entry]
+            if throttler.get_estimated_rate() > rate:
+                # Avoid unnecessary wait period if we pass from fast to slow.
+                throttler.force_estimated_rate(rate)
+            self.entry_to_throttler_map[entry].set_rate(rate)
+
     def _the_watch_callback(self, entry_id: str) -> None:
         """Callback called by the datastore whenever somebody starts watching an entry."""
         entry = self.datastore.get_entry(entry_id)
@@ -214,10 +239,15 @@ class MemoryReader(BaseDeviceHandlerSubmodule):
         elif isinstance(entry, DatastoreRPVEntry):
             self.watched_rpv_entries.add(entry)
 
+        self._update_entry_update_rate(entry, rate=self.datastore.get_effective_update_rate(entry))
+
+    def _the_update_rate_changed_callback(self, entry: DatastoreEntry, new_rate: Optional[float]) -> None:
+        self._update_entry_update_rate(entry, new_rate)
+
     def _the_unwatch_callback(self, entry_id: str) -> None:
         """Callback called by the datastore  whenever somebody stops watching an entry"""
+        entry = self.datastore.get_entry(entry_id)
         if len(self.datastore.get_watchers(entry_id)) == 0:
-            entry = self.datastore.get_entry(entry_id)
             if isinstance(entry, DatastorePointedVariableEntry):
                 self.watched_pointed_var_entries.remove(entry)
             elif isinstance(entry, DatastoreVariableEntry):
@@ -225,12 +255,17 @@ class MemoryReader(BaseDeviceHandlerSubmodule):
             elif isinstance(entry, DatastoreRPVEntry):
                 self.watched_rpv_entries.remove(entry)
 
+        self._update_entry_update_rate(entry, rate=self.datastore.get_effective_update_rate(entry))
+
     def start(self) -> None:
         """Enable the memory reader to poll the devices"""
         self.logger.debug("Start requested")
         if not self.started:
             self.read_type_changed = True
         self.started = True
+
+        for throttler in self.entry_to_throttler_map.values():
+            throttler.reset()
 
     def stop(self) -> None:
         """Stops the memory readers from polling the device"""
@@ -269,6 +304,9 @@ class MemoryReader(BaseDeviceHandlerSubmodule):
         self.entries_in_pending_read_var_request = []
         self.entries_in_pending_read_rpv_request = {}
         self.read_type_changed = True
+
+        for throttler in self.entry_to_throttler_map.values():
+            throttler.reset()
 
         if self.active_raw_read_request is not None:
             self.active_raw_read_request.set_completed(False, None, "Stopping communication with device")
@@ -451,6 +489,14 @@ class MemoryReader(BaseDeviceHandlerSubmodule):
             else:
                 raise NotImplementedError("Unsupported variable type")
 
+            if candidate_entry in self.entry_to_throttler_map:               # Throttling is applied.
+                throttler = self.entry_to_throttler_map[candidate_entry]
+                throttler.process()
+                # if candidate_entry.display_path.endswith('y'):
+                #    print(f"Slow={throttler.estimated_rate_slow}. Fast={throttler.estimated_rate_fast}. since_last_estimation={throttler.consumed_since_last_estimation}")
+                if not throttler.allowed(1):
+                    must_skip = True
+
             candidate_start = candidate_entry.get_address()
             candidate_size = candidate_entry.get_size()
             if not must_skip:
@@ -491,6 +537,9 @@ class MemoryReader(BaseDeviceHandlerSubmodule):
                 clusters_in_request = copy.copy(clusters_candidate)
                 entries_in_request.append(candidate_entry)   # Remember what entries is involved so we can update value once response is received
 
+                if candidate_entry in self.entry_to_throttler_map:
+                    self.entry_to_throttler_map[candidate_entry].consume(1)
+
             if vartype == VarType.AbsoluteAddress:
                 self.var_read_cursor += 1
                 if self.var_read_cursor >= len(self.active_watched_var_entries):
@@ -521,29 +570,45 @@ class MemoryReader(BaseDeviceHandlerSubmodule):
         if self.rpv_read_cursor >= len(self.active_watched_rpv_entries):
             self.rpv_read_cursor = 0
 
-        while len(entries_in_request) < len(self.active_watched_rpv_entries):
+        skipped_entries_count = 0
+        while skipped_entries_count + len(entries_in_request) < len(self.active_watched_rpv_entries):
             next_entry = self.active_watched_rpv_entries[self.rpv_read_cursor]
+            must_skip = False
 
-            candidate_list = entries_in_request + [next_entry]
-            rpv_candidate_list = [x.get_rpv() for x in candidate_list]
-            required_request_payload_size = self.protocol.read_rpv_request_required_size(rpv_candidate_list)
-            required_response_payload_size = self.protocol.read_rpv_response_required_size(rpv_candidate_list)
+            if next_entry in self.entry_to_throttler_map:
+                throttler = self.entry_to_throttler_map[next_entry]
+                throttler.process()
+                if not throttler.allowed(1):
+                    must_skip = True
 
-            if required_request_payload_size > self.max_request_payload_size:
-                break
-            if required_response_payload_size > self.max_response_payload_size:
-                break
+            if must_skip:
+                skipped_entries_count += 1
+            else:
+                candidate_list = entries_in_request + [next_entry]
+                rpv_candidate_list = [x.get_rpv() for x in candidate_list]
+                required_request_payload_size = self.protocol.read_rpv_request_required_size(rpv_candidate_list)
+                required_response_payload_size = self.protocol.read_rpv_response_required_size(rpv_candidate_list)
 
-            # We keep this entry.
-            entries_in_request = candidate_list
+                if required_request_payload_size > self.max_request_payload_size:
+                    break
+                if required_response_payload_size > self.max_response_payload_size:
+                    break
+
+                # We keep this entry.
+                entries_in_request = candidate_list
+                if next_entry in self.entry_to_throttler_map:               # Throttling is applied.
+                    self.entry_to_throttler_map[next_entry].consume(1)
             self.rpv_read_cursor += 1
             if self.rpv_read_cursor >= len(self.active_watched_rpv_entries):
                 cursor_wrapped = True   # Indicates that we finished one round of round-robin for RPV entries. Times to check the next category
                 self.rpv_read_cursor = 0
+                # We don't break here. If we're making wrapping and still have room in the request, why not
+                # ask again for RPVs at the beginning of the list
 
         ids = [x.get_rpv().id for x in entries_in_request]
         request = self.protocol.read_runtime_published_values(ids) if len(ids) > 0 else None
-        return (request, entries_in_request, cursor_wrapped)
+        return (request, entries_in_request,
+                cursor_wrapped)
 
     def _make_next_raw_mem_read_request(self) -> Tuple[Optional[Request], bool]:
         while self.active_raw_read_request is None:

scrutiny/sdk/_api_parser.py
@@ -1423,3 +1423,25 @@ def parse_upload_sfd_data_response(response: api_typing.S2C.UploadSFDData) -> Up
         actual_size=response['actual_size'],
         sfd_info=sfd_info
     )
+
+
+def parse_change_update_rate_response(response: api_typing.S2C.ChangeSubscriptionUpdateRate) -> Dict[str, Optional[float]]:
+    assert isinstance(response, dict)
+    assert 'cmd' in response
+    cmd = response['cmd']
+    assert cmd == API.Command.Api2Client.CHANGE_SUBSCRIPTION_UPDATE_RATE_RESPONSE
+
+    _check_response_dict(cmd, response, 'effective_rates', dict)
+    out_dict: Dict[str, Optional[float]] = {}
+    for k, v in response['effective_rates'].items():
+        if not isinstance(k, str):
+            raise sdk.exceptions.BadResponseError("Server ID given by the server is not a string")
+        if not isinstance(v, (type(None), float, int)) or isinstance(v, bool):
+            raise sdk.exceptions.BadResponseError("Invalid effective rate given by the server")
+
+        if v is not None:
+            out_dict[k] = float(v)
+        else:
+            out_dict[k] = None
+
+    return out_dict

scrutiny/sdk/client.py
@@ -1965,6 +1965,45 @@ class ScrutinyClient:
                 raise sdk.exceptions.TimeoutException(
                     f"Incomplete batch write. {incomplete_count} write requests not completed in {batch.timeout} sec. ")
 
+    def _change_update_rate(self, handle: WatchableHandle, update_rate: Optional[float] = None) -> Optional[float]:
+        validation.assert_type(handle, 'handle', WatchableHandle)
+        update_rate = validation.assert_float_range_if_not_none(update_rate, 'update_rate', 1)
+
+        @dataclass
+        class MutableContainer:
+            effective_rate: Optional[float]
+
+        mutable_ref = MutableContainer(effective_rate=None)
+
+        def wt_change_callback(state: CallbackState, response: Optional[api_typing.S2CMessage]) -> None:
+            if response is not None and state == CallbackState.OK:
+                response = cast(api_typing.S2C.ChangeSubscriptionUpdateRate, response)
+                changes = api_parser.parse_change_update_rate_response(response)
+
+                if len(changes) != 1:
+                    raise sdk.exceptions.BadResponseError(f"Expected 1 rate value in reposne, got {len(changes)}")
+
+                server_id, effective_rate = next(iter(changes.items()))
+                if server_id != handle.server_id:
+                    raise sdk.exceptions.BadResponseError(f"Received an effective update rate for the wrong watchable.")
+
+                mutable_ref.effective_rate = effective_rate
+
+        req = self._make_request(API.Command.Client2Api.CHANGE_SUBSCRIPTION_UPDATE_RATE, {
+            'changes': [
+                {'id': handle.server_id, 'rate': update_rate}  # Single element
+            ],
+        })
+
+        future = self._send(req, wt_change_callback)
+        assert future is not None
+        future.wait()
+
+        if future.state != CallbackState.OK:
+            raise sdk.exceptions.OperationFailure(f"Failed to change the watchable update rate. {future.error_str}")
+
+        return mutable_ref.effective_rate
+
     # === User API ====
 
     def connect(self, hostname: str, port: int, wait_status: bool = True) -> "ScrutinyClient":
@@ -2091,10 +2130,15 @@ class ScrutinyClient:
 
         return cached_watchable
 
-    def watch(self, path: str) -> WatchableHandle:
+    def watch(self, path: str, update_rate: Optional[float] = None) -> WatchableHandle:
         """Starts watching a watchable element identified by its display path (tree-like path)
 
         :param path: The path of the element to watch
+        :param update_rate: The update rate at which the server should update this value.
+            The actual rate may be faster if another client requires a higher update frequency
+            A value of ``None`` indicates that updates should occur as quickly as possible.
+            This rate applies to the server/device communication while :meth:`set_server_throttling<scrutiny.sdk.client.ScrutinyClient.set_server_throttling>`
+            controls the server/client communication
 
         :raises OperationFailure: If the watch request fails to complete
         :raises TypeError: Given parameter not of the expected type
@@ -2102,6 +2146,7 @@ class ScrutinyClient:
         :return: A handle that can read/write the watched element.
         """
         validation.assert_type(path, 'path', str)
+        update_rate = validation.assert_float_range_if_not_none(update_rate, 'update_rate', minval=1)
         cached_watchable = self.try_get_existing_watch_handle(path)
         if cached_watchable:
             return cached_watchable
@@ -2127,8 +2172,10 @@ class ScrutinyClient:
                     self._watchable_storage[watchable._configuration.server_id] = watchable
 
         req = self._make_request(API.Command.Client2Api.SUBSCRIBE_WATCHABLE, {
-            'watchables': [watchable.server_path]  # Single element
+            'watchables': [watchable.server_path],  # Single element
+            'rate': update_rate
         })
+
         future = self._send(req, wt_subscribe_callback)
         assert future is not None
         future.wait()
@@ -2986,7 +3033,7 @@ class ScrutinyClient:
 
         req = self._make_request(API.Command.Client2Api.SET_LINK_CONFIG, {
             'link_type': link_type_api_name,
-            'link_config': link_config._to_api_format()
+            'link_config': link_config._to_api_format() if link_config is not None else None
         })
 
         future = self._send(req, lambda *args, **kwargs: None)

scrutiny/sdk/watchable_handle.py
@@ -321,6 +321,22 @@ class WatchableHandle:
 
         return (val, val_status)
 
+    def change_update_rate(self, update_rate: Optional[float]) -> Optional[float]:
+        """Request the server to change the target update rate for this watchable (optionally set when
+        calling :meth:`watch()<scrutiny.sdk.client.ScrutinyClient.watch>`).
+        When there are multiple clients watching the same watchable, the server applies the fastest required rate.
+
+        :param update_rate: The new polling rate. A value of ``None`` indicates that updates should happen as fast as possible.
+            Must be ``None`` or  greater or equal to 1
+
+        :return: The effective update rate at the moment of change. May be higher or change later if another client requires it.
+
+        :raises TypeError: Given parameter not of the expected type
+        :raises ValueError: Given parameter has an invalid value
+        :raises OperationFailure: If the request fails to complete
+        """
+        return self._client._change_update_rate(self, update_rate)
+
     @property
     @deprecated("Replaced by server_path")
     def display_path(self) -> str:

scrutiny/server/api/API.py
@@ -124,6 +124,7 @@ class API:
             GET_WATCHABLE_INFO = 'get_watchable_info'
             SUBSCRIBE_WATCHABLE = 'subscribe_watchable'
             UNSUBSCRIBE_WATCHABLE = 'unsubscribe_watchable'
+            CHANGE_SUBSCRIPTION_UPDATE_RATE = 'change_subscription_update_rate'
             GET_INSTALLED_SFD = 'get_installed_sfd'
             GET_LOADED_SFD = 'get_loaded_sfd'
             UNINSTALL_SFD = 'uninstall_sfd'
@@ -159,6 +160,7 @@ class API:
             GET_WATCHABLE_INFO_RESPONSE = 'response_get_watchable_info'
             SUBSCRIBE_WATCHABLE_RESPONSE = 'response_subscribe_watchable'
             UNSUBSCRIBE_WATCHABLE_RESPONSE = 'response_unsubscribe_watchable'
+            CHANGE_SUBSCRIPTION_UPDATE_RATE_RESPONSE = 'response_change_subscription_update_rate'
             WATCHABLE_UPDATE = 'watchable_update'
             GET_INSTALLED_SFD_RESPONSE = 'response_get_installed_sfd'
             GET_LOADED_SFD_RESPONSE = 'response_get_loaded_sfd'
@@ -422,6 +424,7 @@ class API:
             self.Command.Client2Api.GET_WATCHABLE_INFO: self.process_get_watchable_info,
             self.Command.Client2Api.SUBSCRIBE_WATCHABLE: self.process_subscribe_watchable,
             self.Command.Client2Api.UNSUBSCRIBE_WATCHABLE: self.process_unsubscribe_watchable,
+            self.Command.Client2Api.CHANGE_SUBSCRIPTION_UPDATE_RATE: self.process_change_subscription_update_rate_change,
             self.Command.Client2Api.GET_INSTALLED_SFD: self.process_get_installed_sfd,
             self.Command.Client2Api.UNINSTALL_SFD: self.process_uninstall_sfd,
             self.Command.Client2Api.LOAD_SFD: self.process_load_sfd,
@@ -933,14 +936,20 @@ class API:
         # datastore callback will write the new values in the API output queue (through the value streamer)
         _check_request_dict(req, req, 'watchables', list)
 
+        rate: Optional[float] = None
+        if 'rate' in req:
+            _check_request_dict(req, req, 'rate', (type(None), float, int))
+            if req['rate'] is not None:
+                rate = float(req['rate'])
+
         # this can throw. Check existence of all watchable before doing anything.
         subscribed = self._make_datastore_detailed_definition_by_display_path(req, req['watchables'])
-
         for path in req['watchables']:
             self.datastore.start_watching(
                 subscribed[path]['id'],
                 watcher=conn_id,    # We use the API connection ID as datastore watcher ID
-                value_change_callback=self.entry_value_change_callback
+                value_change_callback=self.entry_value_change_callback,
+                requested_rate=rate
             )
 
         response: api_typing.S2C.SubscribeWatchable = {
@@ -972,6 +981,34 @@ class API:
             'cmd': self.Command.Api2Client.UNSUBSCRIBE_WATCHABLE_RESPONSE,
             'reqid': self.get_req_id(req),
             'unsubscribed': req['watchables']
+        }
+
+        self.client_handler.send(ClientHandlerMessage(conn_id=conn_id, obj=response))
+
+    #  ===  CHANGE_SUBSCRIPTION_UPDATE_RATE ===
+    def process_change_subscription_update_rate_change(self, conn_id: str, req: api_typing.C2S.ChangeSubscriptionUpdateRate) -> None:
+        # Unsubscribe client from value update of the given datastore entries
+        _check_request_dict(req, req, 'changes', list)
+
+        effective_rates: Dict[str, Optional[float]] = {}
+        # Check existence of all entries before doing anything
+        for change in req['changes']:
+            _check_request_dict(req, change, f'id', str)
+            _check_request_dict(req, change, f'rate', (type(None), float, int))
+
+            rate: Optional[float] = None
+            if change['rate'] is not None:
+                rate = float(change['rate'])
+
+            try:
+                effective_rates[change['id']] = self.datastore.set_update_rate(conn_id, change['id'], rate)
+            except Exception as e:
+                raise InvalidRequestException(req, f'Cannot change update rate : {e}')
+
+        response: api_typing.S2C.ChangeSubscriptionUpdateRate = {
+            'cmd': self.Command.Api2Client.CHANGE_SUBSCRIPTION_UPDATE_RATE_RESPONSE,
+            'reqid': self.get_req_id(req),
+            'effective_rates': effective_rates
         }
 
         self.client_handler.send(ClientHandlerMessage(conn_id=conn_id, obj=response))

scrutiny/server/api/typing.py
@@ -258,6 +258,11 @@ class FileChunk(TypedDict):
     chunk_index: int
 
 
+class SubscriptionUpdateRateChange(TypedDict):
+    id: str
+    rate: Optional[float]
+
+
 class C2S:
     "Client To Server"
     class Echo(BaseC2SMessage):
@@ -305,9 +310,13 @@ class C2S:
 
     class SubscribeWatchable(BaseC2SMessage):
         watchables: List[str]
+        rate: Optional[float]
 
     class UnsubscribeWatchable(BaseC2SMessage):
         watchables: List[str]
+
+    class ChangeSubscriptionUpdateRate(BaseC2SMessage):
+        changes: List[SubscriptionUpdateRateChange]
 
     class SetLinkConfig(BaseC2SMessage, DeviceCommLinkDef):
         pass
@@ -438,6 +447,9 @@ class S2C:
     class UnsubscribeWatchable(BaseS2CMessage):
         unsubscribed: List[str]
 
+    class ChangeSubscriptionUpdateRate(BaseS2CMessage):
+        effective_rates: Dict[str, Optional[float]]
+
     class WatchableUpdate(BaseS2CMessage):
         updates: List[WatchableUpdateRecord]
 
@@ -549,6 +561,7 @@ C2SMessage = Union[
     C2S.UninstallSFD,
     C2S.SubscribeWatchable,
     C2S.UnsubscribeWatchable,
+    C2S.ChangeSubscriptionUpdateRate,
     C2S.WriteValue,
     C2S.RequestDataloggingAcquisition,
     C2S.ReadDataloggingAcquisitionContent,
@@ -578,6 +591,7 @@ S2CMessage = Union[
     S2C.GetWatchableInfo,
     S2C.SubscribeWatchable,
     S2C.UnsubscribeWatchable,
+    S2C.ChangeSubscriptionUpdateRate,
     S2C.WatchableUpdate,
     S2C.WriteValue,
     S2C.WriteCompletion,

scrutiny/server/api/value_streamer.py
@@ -91,7 +91,7 @@ class ValueStreamer:
     def _set_actual_throttling_measurement(self, conn_id: str, rate: float) -> None:
         """For unit testing"""
         throttler = self._conn_data[conn_id].stream_rate_throttler
-        throttler.set_estimated_rate_for_testing(rate)
+        throttler.force_estimated_rate(rate)
 
     def publish(self, entry: DatastoreEntry, conn_id: str) -> None:
         """ inform the value streamer that a new value should be published.

commit 1456be84edad95289d8062ae5a4359389fe5a147

Server : Fix CPU usage problem when all watched entries are rate limited (#370)
Fixes #355, caused by #319

Server would never sleep when all the watched entries are rate limited (GUI watch window request 15 update/sec).
``MemoryReader::would_send_data()`` did not consider the throttling state

scrutiny/server/device/submodules/memory_reader.py
@@ -164,6 +164,9 @@ class MemoryReader(BaseDeviceHandlerSubmodule):
     read_type_changed: bool
     """A flag that indicates if the read type change in the last process iteration"""
     entry_to_throttler_map: Dict[DatastoreEntry, Throttler]
+    """A dict mapping each entry to their throttler if they are rate limited"""
+    _last_process_produced_no_request:bool
+    """A flag to remember if calling process() failed to produce a request. Used to detect throttling condition to avoid keeping the CPU alive """
 
     def __init__(self, protocol: Protocol, dispatcher: RequestDispatcher, datastore: Datastore, request_priority: int):
         self.logger = logging.getLogger(self.__class__.__name__)
@@ -304,6 +307,7 @@ class MemoryReader(BaseDeviceHandlerSubmodule):
         self.entries_in_pending_read_var_request = []
         self.entries_in_pending_read_rpv_request = {}
         self.read_type_changed = True
+        self._last_process_produced_no_request = False
 
         for throttler in self.entry_to_throttler_map.values():
             throttler.reset()
@@ -335,6 +339,11 @@ class MemoryReader(BaseDeviceHandlerSubmodule):
         if self.pending_request is not None:
             return False
 
+        # Avoid spin waiting when all entries are throttled.
+        # We could scan entry_to_throttler_map fully, but this would be CPU intensive.
+        if self._last_process_produced_no_request:
+            return False
+
         return (
             len(self.watched_var_entries) > 0
             or len(self.watched_rpv_entries) > 0
@@ -352,13 +361,15 @@ class MemoryReader(BaseDeviceHandlerSubmodule):
             return
 
         read_type_considered: Set[ReadType] = set()
+        self._last_process_produced_no_request = False
+        no_request_beginning = (self.pending_request is None)
+
         while self.pending_request is None and len(read_type_considered) < len(ReadType):
             read_type_considered.add(self.actual_read_type)
 
             # We want to read everything in a round robin scheme. But we need to read memory and RPV as much without discrimination
             # So we need to do   ReadMem1, ReadMem2, ReadMem3, ReadRPV1, ReadRPV2  **WRAP**  ReadMem1, ReadMem2, etc
             # Also, pointed var needs to be read after vars with addresses (they include pointers)
-
             if self.actual_read_type == ReadType.Variable:
                 if self.read_type_changed:
                     self.active_watched_var_entries = sorted(self.watched_var_entries, key=lambda entry: entry.get_address_or_zero())
@@ -438,6 +449,11 @@ class MemoryReader(BaseDeviceHandlerSubmodule):
             else:
                 raise Exception('Unknown read type.')
 
+        no_request_end = (self.pending_request is None)
+
+        if no_request_beginning and no_request_end:
+            self._last_process_produced_no_request = True
+
     def _dispatch(self, request: Request, success_params: Any = None, failure_params: Any = None) -> None:
         """Sends a request to the request dispatcher and assign the corrects completion callbacks"""
         self.dispatcher.register_request(
@@ -492,8 +508,6 @@ class MemoryReader(BaseDeviceHandlerSubmodule):
             if candidate_entry in self.entry_to_throttler_map:               # Throttling is applied.
                 throttler = self.entry_to_throttler_map[candidate_entry]
                 throttler.process()
-                # if candidate_entry.display_path.endswith('y'):
-                #    print(f"Slow={throttler.estimated_rate_slow}. Fast={throttler.estimated_rate_fast}. since_last_estimation={throttler.consumed_since_last_estimation}")
                 if not throttler.allowed(1):
                     must_skip = True
 